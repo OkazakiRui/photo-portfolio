b26c048b22e770190565a309486ddb5b9cd63a33
RuiOkazaki <okazaki200226@gmail.com>
2022-04-06 09:31:21 +0900

    add: テーマファイルの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <Heading color="pink">Hello, Next.js with Chakra UI</Heading>;
+  return <Heading>Hello, Next.js with Chakra UI</Heading>;
 };
 
 export default Home;
