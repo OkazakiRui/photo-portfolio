9229948f09d05cf3256e3e5a04f453fc27e9d855
RuiOkazaki <okazaki200226@gmail.com>
2022-04-07 12:35:59 +0900

    chore: colorModeをアプリを閉じた場合でも設定を引き継ぐようにした

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript, theme } from '@chakra-ui/react';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
