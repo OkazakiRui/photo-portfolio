506013a030d635dea0cbc56bd91795b4d1899bf9
RuiOkazaki <okazaki200226@gmail.com>
2022-04-08 21:03:57 +0900

    feat: 投稿された写真一覧を取得する関数を作成した

---
+++ src/libs/apiClient.ts
@@ -0,0 +1,21 @@
+import aspida from '@aspida/fetch';
+import api from 'apis/$api';
+
+export const endPoint = process.env.BASE_API_URL;
+export const apiKey = process.env.X_MICROCMS_API_KEY;
+
+if (endPoint === undefined)
+  throw new Error('環境変数に BASE_API_URL をセットしてください');
+if (apiKey === undefined)
+  throw new Error('環境変数に X_MICROCMS_API_KEY をセットしてください');
+
+const config = {
+  headers: {
+    'X-MICROCMS-API-KEY': apiKey,
+  },
+};
+
+const fetchClient = api(aspida(fetch, { baseURL: endPoint }));
+
+export const getAllPost = () =>
+  fetchClient.post.$get({ config, query: { limit: 1000 } });
