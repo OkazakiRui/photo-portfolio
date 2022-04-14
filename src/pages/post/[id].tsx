e91d525be05170cde16b060987ebdf56ec01604c
RuiOkazaki <okazaki200226@gmail.com>
2022-04-14 15:03:02 +0900

    feat: 詳細ページを作成した

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -3,9 +3,10 @@ import { getAllPost, getPostById } from 'libs/apiClient';
 import type { NextPage } from 'next';
 import Logo from 'components/Logo';
 import NextImage from 'next/image';
-import { Box, Center, Heading, Button } from '@chakra-ui/react';
+import { Box, Center, Heading, Button, Flex } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
 
 type Props = {
   post: PostItem;
@@ -27,6 +28,7 @@ const Post: NextPage<Props> = ({ post, postsData }) => {
       <Box as="header" pos="relative" zIndex={10}>
         <Logo />
       </Box>
+      {/* <Box mb={[4, 6]}> */}
       <Box>
         <Center
           h={[
@@ -34,7 +36,7 @@ const Post: NextPage<Props> = ({ post, postsData }) => {
             'calc(100vh - 3rem - 0.25rem - 1.25rem * 1.2 - 0.5rem - 0.875rem * 1.5)',
           ]}
         >
-          <Box className="imageContainer" h="96%" w="78%">
+          <Box className="imageContainer" h={['84%', '96%']} w={['98%', '78%']}>
             <NextImage
               src={
                 isHorizontal
@@ -59,66 +61,92 @@ const Post: NextPage<Props> = ({ post, postsData }) => {
           {'title' in post ? post.title : 'No Title'}
         </Heading>
       </Box>
-      {prev && (
-        <NextLink
-          href={`/post/${prev.id}${
-            'category' in router.query ? `?category=${queryGenre}` : ''
-          }`}
-          passHref
-        >
-          <Button
-            as="a"
-            href="/"
-            pos="absolute"
-            top={0}
-            left={0}
-            height="full"
-            zIndex={1}
-            width="5rem"
-            bg="transparent"
-            borderRadius="0"
-            _hover={{
-              bg: ['none', 'accent'],
-            }}
-            _after={{
-              content: '"Prev"',
-              position: 'absolute',
-              transform: 'rotate(90deg)',
-              left: 6,
-            }}
-          />
-        </NextLink>
-      )}
-      {next && (
-        <NextLink
-          href={`/post/${next.id}${
-            'category' in router.query ? `?category=${queryGenre}` : ''
-          }`}
-          passHref
-        >
-          <Button
-            as="a"
-            href="/"
-            pos="absolute"
-            top={0}
-            right={0}
-            height="full"
-            zIndex={1}
-            width="5rem"
-            bg="transparent"
-            borderRadius="0"
-            _hover={{
-              bg: ['none', 'accent'],
-            }}
-            _after={{
-              content: '"Next"',
-              position: 'absolute',
-              transform: 'rotate(90deg)',
-              right: 5,
-            }}
-          />
-        </NextLink>
-      )}
+      <Flex justify="right" pos={['absolute', 'static']} bottom={5} right={3}>
+        {prev && (
+          <NextLink
+            href={`/post/${prev.id}${
+              'category' in router.query ? `?category=${queryGenre}` : ''
+            }`}
+            passHref
+          >
+            <Button
+              as="a"
+              href="/"
+              pos={['static', 'absolute']}
+              top={0}
+              left={0}
+              height={['fit-content', 'full']}
+              px={0}
+              zIndex={1}
+              width={['fit-content', '5rem']}
+              bg="transparent"
+              borderRadius="0"
+              _hover={{
+                bg: ['none', 'accent'],
+              }}
+              _after={{
+                content: ['""', '"Prev"'],
+                position: 'absolute',
+                transform: ['rotate(0deg)', 'rotate(90deg)'],
+                left: 6,
+              }}
+            >
+              <ArrowBackIcon
+                display={['block', 'none']}
+                p={1}
+                outline="1px solid"
+                outlineColor="body"
+                borderRadius="50%"
+                w={6}
+                h={6}
+                _hover={{ color: 'accent', outlineColor: 'accent' }}
+              />
+            </Button>
+          </NextLink>
+        )}
+        {next && (
+          <NextLink
+            href={`/post/${next.id}${
+              'category' in router.query ? `?category=${queryGenre}` : ''
+            }`}
+            passHref
+          >
+            <Button
+              as="a"
+              href="/"
+              pos={['static', 'absolute']}
+              top={0}
+              right={0}
+              height={['fit-content', 'full']}
+              px={0}
+              zIndex={1}
+              width={['fit-content', '5rem']}
+              bg="transparent"
+              borderRadius="0"
+              _hover={{
+                bg: ['none', 'accent'],
+              }}
+              _after={{
+                content: ['""', '"Next"'],
+                position: 'absolute',
+                transform: ['rotate(0deg)', 'rotate(90deg)'],
+                right: 5,
+              }}
+            >
+              <ArrowForwardIcon
+                display={['block', 'none']}
+                p={1}
+                outline="1px solid"
+                outlineColor="body"
+                borderRadius="50%"
+                w={6}
+                h={6}
+                _hover={{ color: 'accent', outlineColor: 'accent' }}
+              />
+            </Button>
+          </NextLink>
+        )}
+      </Flex>
     </Box>
   );
 };
