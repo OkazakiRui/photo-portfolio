c7a16d3f3320b61d2c695c4a60cbaee5c6a1b4b8
RuiOkazaki <okazaki200226@gmail.com>
2022-04-16 09:42:39 +0900

    feat: default seo を設定した

---
+++ src/libs/next-seo.config.ts
@@ -0,0 +1,25 @@
+export default {
+  titleTemplate: '%s | RuiOkazaki Photograph Portfolio',
+  defaultTitle: 'RuiOkazaki Photograph Portfolio',
+  additionalMetaTags: [
+    {
+      property: 'dc:creator',
+      content: 'Rui Okazaki',
+    },
+    {
+      name: 'application-name',
+      content: 'RuiOkazaki-PhotographPortfolio',
+    },
+  ],
+  openGraph: {
+    url: 'https://photograph-portfolio-gamma.vercel.app/',
+    type: 'website',
+    locale: 'ja_JP',
+    site_name: 'RuiOkazaki-PhotographPortfolio',
+  },
+  twitter: {
+    handle: '@yoruhanemutaiyo',
+    site: '@yoruhanemutaiyo',
+    cardType: 'summary_large_image',
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'theme';
+import { DefaultSeo } from 'next-seo';
+import seoConfig from 'libs/next-seo.config';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider theme={theme}>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading  */}
+    <DefaultSeo {...seoConfig} />
     <Component {...pageProps} />
   </ChakraProvider>
 );
