b79a107aae04f7286fd6b547b66603d71a35bdf8
RuiOkazaki <okazaki200226@gmail.com>
2022-04-16 14:08:49 +0900

    feat: ga4 を有効化した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,17 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'theme';
 import { DefaultSeo } from 'next-seo';
 import seoConfig from 'libs/next-seo.config';
+import GoogleTagManager, {
+  GoogleTagManagerId,
+} from 'components/GoogleTagManager';
+import googleTagManagerId from 'utils/gtm';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider theme={theme}>
     <DefaultSeo {...seoConfig} />
+    <GoogleTagManager
+      googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+    />
     <Component {...pageProps} />
   </ChakraProvider>
 );
