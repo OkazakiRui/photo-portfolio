bcb6da0f81194bc016862bc5cd0381936bf27ec6
RuiOkazaki <okazaki200226@gmail.com>
2022-04-16 14:09:40 +0900

    feat: jsが使えなかった時の表示を追加した

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript, theme } from '@chakra-ui/react';
+import googleTagManagerId from 'utils/gtm';
 
 export default class Document extends NextDocument {
   render() {
@@ -12,6 +13,18 @@ export default class Document extends NextDocument {
           />
         </Head>
         <body>
+          <noscript
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              />`,
+            }}
+          />
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
